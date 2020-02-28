71403f92ac14b041188e3fa625882edb6cd87c8e
jsun1324 <jsun@eastsideprep.org>
2020-02-28 10:34:10 -0800

    More Progress

--- com.mycompany_GraphingCalculator_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/graphingcalculator/App.java
+++ com.mycompany_GraphingCalculator_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/graphingcalculator/App.java
@@ -1,18 +1,20 @@
 package com.mycompany.graphingcalculator;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-//import javax.script.ScriptEngineManager;
-//import javax.script.ScriptEngine;
-//import javax.script.ScriptException;
 
 /**
  * JavaFX App
@@ -31,17 +33,16 @@ public class App extends Application {
 //        testing
         final ComboBox emailComboBox = new ComboBox();
         emailComboBox.getItems().addAll(
-            "jacob.smith@example.com",
-            "isabella.johnson@example.com",
-            "ethan.williams@example.com",
-            "emma.jones@example.com",
-            "michael.brown@example.com"  
+                "y=mx+b",
+                "y=ax^2+bx+c",
+                "y=x^2",
+                "y=sin(x)",
+                "y=cos(x)"
         );
-        
-        
+
         Group root = new Group();
         root.getChildren().add(emailComboBox);
-        
+
         int a = 100;
         int b = 100;
         int c = 100;
@@ -53,38 +54,54 @@ public class App extends Application {
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         //creating the chart
-        final LineChart<Number,Number> lineChart = 
-                new LineChart<Number,Number>(xAxis,yAxis);
-                
+        final LineChart<Number, Number> lineChart
+                = new LineChart<Number, Number>(xAxis, yAxis);
+
         lineChart.setTitle("Graph of Equation");
         //defining a series
         XYChart.Series series = new XYChart.Series();
-        
+
         //populating the series with data
-        
         for (int i = -100; i < 100; i++) {
-            series.getData().add(new XYChart.Data(i, a*Math.pow(i, powa)+b*Math.pow(i, powb)+c*Math.pow(i, powc)));
-            
+            series.getData().add(new XYChart.Data(i, a * Math.pow(i, powa) + b * Math.pow(i, powb) + c * Math.pow(i, powc)));
+
         }
         
-        Scene scene  = new Scene(root,800,600);
-        lineChart.getData().add(series);
-        root.getChildren().add(lineChart);
+        
+        
+        TextField textField = new TextField ();
+
+        Button graph = new Button("Graph");
+
+        graph.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                if ((textField.getText() != null && !textField.getText().isEmpty())) {
+                    if(emailComboBox.getValue().equals("y=mx+b")){
+                        System.out.println("LIN " + textField.getText());
+                    }else if(emailComboBox.getValue().equals("y=ax^2+bx+c")){
+                        System.out.println("QUAD " + textField.getText());
+                    }else if(emailComboBox.getValue().equals("y=x^2")){
+                        System.out.println("POW " + textField.getText());
+                    }else if(emailComboBox.getValue().equals("y=sin(x)")){
+                        System.out.println("SIN " + textField.getText());
+                    }else if(emailComboBox.getValue().equals("y=cos(x)")){
+                        System.out.println("COS " + textField.getText());
+                    }
+                }
+            }
+        });
+
+        VBox vbox = new VBox(emailComboBox, textField, graph, lineChart);
+        Scene scene = new Scene(vbox, 800, 600);
+
         //SWITCH FROM GROUP TO VBOX TO FIX LAYOUT
-       
         stage.setScene(scene);
         stage.show();
     }
-    
 
     public static void main(String[] args) {
         launch();
     }
-//    public static void script(String[] args) throws ScriptException {
-//    ScriptEngineManager mgr = new ScriptEngineManager();
-//    ScriptEngine engine = mgr.getEngineByName("JavaScript");
-//    String foo = "40+2";
-//    System.out.println(engine.eval(foo));
-//    } 
 
 }
